247b235e14c9ecf199fea14eba531a698e636aa8
Alam García Roldán <garcia76@avaya.com>
2018-10-16 13:18:45 -0500

    Add project files.

---
+++ ClientSDKAvaya/ApplicationClientDelegate.cs
@@ -0,0 +1,30 @@
+﻿using Avaya.ClientServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSDKAvaya
+{
+    public class ApplicationClientDelegate
+    {
+       public void client_UserCreated(object sender, UserEventArgs e)
+        {
+            Console.WriteLine("Usuario Creado");
+
+        }
+
+        public void client_UserRemoved(object sender, UserEventArgs e)
+        {
+            Console.WriteLine("Se ha eliminado Usuario");
+
+        }
+
+        public void client_ShutdownCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Shutdown");
+
+        }
+    }
+}
---
+++ ClientSDKAvaya/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Avaya.ClientServices;
+using ClientSDKAvaya;
+
+namespace ClientSDKAvaya
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConfigureSDK();
+
+        }
+
+        public void ConfigureSDK()
+        {
+            ClientConfiguration clientConfiguration = new ClientConfiguration("C:Avaya\avayaclientservices.txt", "TestApplication", "Avaya", "Windows", "1.0", "1", "Avaya");
+            string userAgentInstanceId;
+
+            userAgentInstanceId = Guid.NewGuid().ToString();
+            clientConfiguration.UserAgentInstanceId = userAgentInstanceId;
+
+            ApplicationClientDelegate clientDelegate = new ApplicationClientDelegate();
+            Client client = new Client(configuration: clientConfiguration);
+            client.UserCreated += new EventHandler<UserEventArgs>
+                (clientDelegate.client_UserCreated);
+            client.UserRemoved += new EventHandler<UserEventArgs>
+                (clientDelegate.client_UserRemoved);
+            client.ShutdownCompleted += new EventHandler
+                (clientDelegate.client_ShutdownCompleted);
+            UserConfiguration userConfiguration = new UserConfiguration();
+            SipUserConfiguration sipConfiguration = userConfiguration.SipUserConfiguration;
+            sipConfiguration.Enabled = true;
+            sipConfiguration.UserId = "garcia76@avaya.com";   // Provided by your administrator
+            sipConfiguration.Domain = "net.avaya.com"; // Provideds by your administrator
+            SignalingServer signalingServer = new SignalingServer(Avaya.ClientServices.TransportType.Automatic ,
+    "sip-na1.avaya.com", // Provided by your administrator 
+    5061,         // Provided by your administrator 
+    Avaya.ClientServices.FailbackPolicy.Automatic);                // Allow the Client SDK to manage failback
+            sipConfiguration.ConnectionPolicy = new ConnectionPolicy(signalingServer);
+            sipConfiguration.CredentialProvider = this;
+            userConfiguration.SipUserConfiguration = sipConfiguration;
+
+        }
+    }
+}
